f909a05f59bb28f43e45d04085d1e5d89ec21886
Michael Beloff <michael.a.beloff@gmail.com>
2018-12-10 11:09:26 +1000

    color changes, animate gradient

---
+++ js/gradient.js
@@ -0,0 +1,7 @@
+let deg = 0
+let el = document.querySelector('.face-bg')
+let tick = () => {
+    el.style.background = `background linear-gradient(${(deg++ / 2)}deg, #f03f57,#f03f57, #ff8965, #f03f57) 0 75% / 200% 200%`
+    requestAnimationFrame(tick)
+}
+requestAnimationFrame(tick)
